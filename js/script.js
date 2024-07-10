08ce186b2b18bf4b15340e25c6ebc9d22759d758
I1K <karmakazmenko@gmail.com>
2024-07-10 17:43:39 +0200

    added about.html + some visual fixes

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let menu__open = document.querySelector('#menu__open')
+let menu = document.querySelector('#menu')
+let menu__spans = document.querySelectorAll('#menu__open span')
+
+
+menu__open.onclick = () => {
+    menu.classList.toggle('opened')
+    document.body.classList.toggle('prevent-scroll')
+    menu__open.classList.toggle('transparent')
+    for (let menu__span of menu__spans) {
+        menu__span.classList.toggle('dark')
+    }
+
+}
